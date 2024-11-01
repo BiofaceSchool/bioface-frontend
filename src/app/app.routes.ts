422041a6c3cce098401850c8900266b668c28db8
AnthonyNunez500 <u202119276@upc.edu.pe>
2024-11-01 16:54:25 -0500

    Base routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CampusScreenComponent } from './components/campus-screen/campus-screen.component';
+import { ProfileScreenComponent } from './components/profile-screen/profile-screen.component';
+import { FacultyScreenComponent } from './components/faculty-screen/faculty-screen.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HelpScreenComponent } from './components/help-screen/help-screen.component';
+import { HomeScreenComponent } from './components/home-screen/home-screen.component';
+import { LoginScreenComponent } from './components/login-screen/login-screen.component';
+import { RegisterScreenComponent } from './components/register-screen/register-screen.component';
+import { SearchScreenComponent } from './components/search-screen/search-screen.component';
 
-export const routes: Routes = [];
+
+const routes: Routes = [
+    {path: 'home', component: HomeScreenComponent},
+    {path: 'login', component: LoginScreenComponent},
+  ];
+  
+  @NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
+  })
+  export class AppRoutingModule {}
