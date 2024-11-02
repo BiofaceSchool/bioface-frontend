192c7f14e290a723bf6f48346bc5f59e65a239c3
Arack <arack.rs@gmail.com>
2024-11-02 18:34:32 -0500

    feat: add register styles

--- src/app/components/register-screen/register-screen.component.ts
+++ src/app/components/register-screen/register-screen.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, signal} from '@angular/core';
+import {MatButtonModule} from "@angular/material/button";
+import {MatCardModule} from "@angular/material/card";
+import {MatSelectModule} from "@angular/material/select";
+import {MatInputModule} from "@angular/material/input";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatIconModule} from "@angular/material/icon";
+import {FormsModule} from '@angular/forms';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 @Component({
   selector: 'app-register-screen',
   standalone: true,
-  imports: [],
+  imports: [
+    MatButtonModule,
+    MatCardModule,
+    MatSelectModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatIconModule,
+    FormsModule,
+    MatCheckboxModule
+  ],
   templateUrl: './register-screen.component.html',
   styleUrl: './register-screen.component.css'
 })
 export class RegisterScreenComponent {
-
+  hide = signal(true);
+  form: boolean | undefined;
+  clickEvent(event: MouseEvent) {
+    this.hide.set(!this.hide());
+    event.stopPropagation();
+  }
 }
